2b869590cea90732c24e3a970bd746c6038f0b14
kelly mweu <mweukelly@gmail.com>
2023-08-20 14:23:19 +0300

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffer
+}
